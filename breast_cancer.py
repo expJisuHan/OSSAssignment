59e3804ca67ed4bf2658d7f09711c7424d283ff6
Han Jisu <ky6806@gmail.com>
2026-06-03 22:29:56 +0900

    Train model : 데이터 정규화 적용 및 모델 학습 성능 개선

--- breast_cancer.py
+++ breast_cancer.py
@@ -1,6 +1,7 @@
 from sklearn.datasets import load_breast_cancer
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import accuracy_score
 
 data = load_breast_cancer()
@@ -19,6 +20,11 @@ X_train, X_test, y_train, y_test = train_test_split(
     test_size=0.2,
     random_state=42
 )
+
+scaler = StandardScaler()
+
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
 
 model = LogisticRegression(max_iter=5000)
 model.fit(X_train, y_train)
